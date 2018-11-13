cc4fa8da7779a379bec66e57db4958c7d5096498
seductorAmadeus <seductorAmadeus@gmail.com>
2018-11-13 09:31:18 +0300

    Initial commit

---
+++ lab1/list1.py
@@ -0,0 +1,42 @@
+# 1.
+# Вх: список строк, Возвр: кол-во строк
+# где строка > 2 символов и первый символ == последнему
+
+def me(words):
+    count = 0
+    for string in words:
+        if string.endswith(string[:1]) and len(string) > 2:
+            count += 1
+    return count
+
+
+# 2.
+# Вх: список строк, Возвр: список со строками (упорядочено)
+# за искл всех строк начинающихся с 'x', которые попадают в начало списка.
+# ['tix', 'xyz', 'apple', 'xacadu', 'aabbbccc'] -> ['xacadu', 'xyz', 'aabbbccc', 'apple', 'tix']
+def fx(words):
+    words.sort()
+    position = 0
+    for string in words:
+        if string.startswith('x'):
+            temp = words.pop(words.index(string))
+            words.insert(position, temp)
+            position += 1
+    xlist = words
+    return xlist
+
+
+# 3.
+# Вх: список непустых кортежей,
+# Возвр: список сортир по возрастанию последнего элемента в каждом корт.
+# [(1, 7), (1, 3), (3, 4, 5), (2, 2)] -> [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+
+
+if __name__ == '__main__':
+    #  test №1
+    test = me(['f2f', 'afeaf', 'f2ff', 'eafaefaefafe'])
+    print(test)
+
+    test1 = fx(['tix', 'xyz', 'apple', 'xacadu', 'aabbbccc', 'xzfqfe3', 'felfafae', 'aaeaaaa', 'xaaaaaaaa'])
+    print(test1)
+#  test №2
