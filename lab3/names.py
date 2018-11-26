127275a5fff2c3c98f9a4665c17cb6ac7f2edd27
seductorAmadeus <seductorAmadeus@gmail.com>
2018-11-26 14:27:01 +0300

    added lab3, but file contains some problem

--- lab3/names.py
+++ lab3/names.py
@@ -1,23 +1,92 @@
+import re
 import sys
 
 
-def extr_name(filename):
+def untag(string: str) -> str:
+
     """
-    Вход: nameYYYY.html, Выход: список начинается с года, продолжается имя-ранг в алфавитном порядке.
-    '2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' и т.д.
+    Вставляет вместо html-тегов пробел (" ")
+    :param string: строка, которую нужно очистить
+    :return: очищенная строка
     """
-    return
+
+    clean_pattern = re.compile(r'<.*?>')
+    clean_text = re.sub(clean_pattern, ' ', string)
+    return clean_text
+
+
+def get_names(html: str) -> list:
+    """
+    Имена расположены в теге <td>...</td> при этом имеется формат:
+    <td>rating</td> <td>male_name</td> <td>female_name</td>
+    Поэтому сразу ищем такую строку в html документе
+    :param html: текст в формате html
+    :return: список с вложенным массивом в формате [rate, male, female]
+    """
+    result = []
+    for names in re.findall(r"<td>\w*</td><td>\w*</td><td>\w*</td>", html):
+        result.append(untag(names).strip().split())
+    return result
+
+
+
+def extr_name(filename: str) -> dict:
+    """
+    Обрабатывает html-файл и возвращает словарь с ключами, которые сожержат:
+    all_names - сортированный по алфавиту массив всех имен с указанием рейтинга
+    (Сначала перед именами записывается год, полученный из имени файла)
+    top_male_names - топ 10 мужских имен
+    top_female_names - топ 10 женских имен
+    :param filename: имя файла или путь к нему
+    :return: словарь содержащий три ключа (all_names, top_male_names, top_female_names)
+    """
+    html = open(filename, "r").read()
+
+    # Получаем год из имени файла
+    year = re.findall(r"\d+", filename)[0]
+    top_male_names = []
+    top_female_names = []
+    all_names = [year]
+    list_names = get_names(html)
+    for line in list_names:
+        rate = line[0]
+        male_name = line[1]
+        female_name = line[2]
+
+        all_names.append(male_name + " " + rate)
+        all_names.append(female_name + " " + rate)
+
+        if int(rate) <= 10:
+            top_male_names.append(male_name)
+            top_female_names.append(female_name)
+
+    return {"all_names": sorted(all_names), "top_male_names": top_male_names, "top_female_names": top_female_names}
 
 
 def main():
+    """
+    Подразумевается, что имя входного файла и его содержимое строго типизированы.
+    """
     args = sys.argv[1:]
     if not args:
         print('use: [--file] file [file ...]')
         sys.exit(1)
 
-    # для каждого переданного аргументом имени файла, вывести имена  extr_name
+    for file in args:
+        names_dict = extr_name(file)
 
-    # напечатать ТОП-10 муж и жен имен из всех переданных файлов
+        for name in names_dict["all_names"]:
+            print(name, end=", ")
+
+        print("\n")
+        print("Топ мужских имён:")
+        for male_name in names_dict["top_male_names"]:
+            print(male_name, end=", ")
+
+        print("\n")
+        print("Топ женских имён:")
+        for female_name in names_dict["top_female_names"]:
+            print(female_name, end=", ")
 
 
 if __name__ == '__main__':
