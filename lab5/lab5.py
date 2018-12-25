2ac8b287df1a6f79f26b629381feddcca8f4a565
seductorAmadeus <seductorAmadeus@gmail.com>
2018-12-25 15:16:34 +0300

    some fixes

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -5,21 +5,22 @@ from random import shuffle
 import requests
 import re
 
+entry_point = Flask(__name__, static_url_path='/static')
 
-def get_names(filename):
+
+def read_names_from_file(filename):
     f = open(filename, 'r')
     data = f.read()
     return list(map(lambda x: x.split('(')[0].strip(), data.splitlines()))
 
 
-names = get_names("names.txt")
+names = read_names_from_file("names.txt")
 picture_urls = dict()
 
 
 def get_urls(name):
     if name not in picture_urls.keys():
-        print("Building img cache for " + name)
-        picture_urls[name] = build_links_for_name(name)
+        picture_urls[name] = get_links(name)
     return picture_urls[name]
 
 
@@ -29,49 +30,46 @@ def get_random_name(lower_bound):
     return entry
 
 
-app = Flask(__name__, static_url_path='/static')
-
-
-@app.route('/randomname')
-def randomname():
-    return get_random_name(99)
-
-
-@app.route("/")
+@entry_point.route("/")
 def hello():
     return render_template('index.html')
 
 
-@app.route("/game/<up_to>")
-def game(up_to="9"):
-    up_to = int(up_to)
-    idx_to_guess = random.randint(0, up_to)
-    name_options = names[:up_to]
+@entry_point.route("/start/<level_param>")
+def game(level_param="low"):
+    level_param = switch_level_to_int(level_param)
+    idx_to_guess = random.randint(0, level_param)
+    name_options = names[:level_param]
     shuffle(name_options)
-    image_links = get_urls(names[idx_to_guess])[:5]
+    image_links = get_urls(names[idx_to_guess])[:3]
     shuffle(image_links)
     return render_template('game.html',
-                           difficulty="1-" + str(up_to + 1) + " names",
+                           difficulty="1-" + str(level_param + 1) + " names",
                            names=name_options,
                            name_index=idx_to_guess,
                            image_links=image_links)
 
 
-@app.route("/guess/<id>/<name>")
+@entry_point.route("/guess/<id>/<name>")
 def guess(id="-1", name=""):
     id = int(id)
     if id == -1:
-        return "Bad rq"
-    print("Someone is guessing that " + str(id) + " is " + name + "")
+        return "invalid request"
     if names[id] == name:
-        print("This is true")
         return "True"
-    print("Actually, " + name + " is " + str(names.index(name)))
-    print("And, " + str(id) + " is " + names[id])
     return "False"
 
 
-def build_links_for_name(name):
+def switch_level_to_int(argument):
+    switcher = {
+        "low": 9,
+        "medium": 49,
+        "high": 99,
+    }
+    return switcher.get(argument, "invalid level")
+
+
+def get_links(name):
     url = 'https://www.google.com/search?q=' + name + '&client=firefox-b-ab&tbm=isch&tbo=u&source=univ&sa=X&ved=2ahUKEwjN5fWwxu3eAhUQmIsKHeA-D90QsAR6BAgEEAE'
     headers = {
         'User-Agent': 'Nokia5250/10.0.011 (SymbianOS/9.4; U; Series60/5.0 Mozilla/5.0; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/525 (KHTML, like Gecko) Safari/525 3gpp-gba'}
@@ -90,4 +88,4 @@ def build_links_for_name(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    entry_point.run(debug=True)
